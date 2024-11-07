4d1c67d91e39d1ed9db14fabf209db6d751d2757
Nayanika0428 <Nayanika0304@gmail.com>
2024-11-08 00:44:29 +0530

    Updated Q5 and Q6

--- Case Study #1-Danny's Dinner/DannysDinerSols.sql
+++ Case Study #1-Danny's Dinner/DannysDinerSols.sql
@@ -31,11 +31,41 @@ group by m.product_name
 order by Number_of_times_bought desc 
 limit 1;
 
-# 5. Which item was the most popular for each customer?
-select customer_id, product_id , count(product_id) as p from sales group by customer_id, product_id;
 
-Select customer_id, product_id,
-count(product_id) over (partition by customer_id, product_id) as count 
-from sales;
+
+
+# 5. Which item was the most popular for each customer?
+with cte1 as (
+select customer_id, product_id , count(product_id) as num_times, 
+dense_rank() over (partition by customer_id order by count(product_id) desc) as Rn
+from sales
+group by customer_id, product_id)
+
+select c.customer_id, c.product_id, m.product_name, c.num_times
+from cte1 c join menu m
+on c.product_id = m.product_id 
+where c.Rn = 1
+order by c.customer_id;
+
+
+
+# 6. Which item was purchased first by the customer after they became a member?
+with cte as (
+select s.customer_id , s.product_id, me.product_name, s.order_date, m.join_date
+from members m left join sales s 
+on s.customer_id =m.customer_id 
+join menu me
+on s.product_id =me.product_id
+where s.order_date >= m.join_date 
+order by customer_id)
+
+select R.customer_id, R.product_name, R.order_date, R.join_date 
+from 
+(select *,  
+Rank() over(partition by customer_id order by order_date) as Rn 
+from cte) as R
+where R.Rn =1;
+
+
 
 
